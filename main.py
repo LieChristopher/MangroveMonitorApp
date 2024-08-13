42ae65e97caffef8a9e43077b45324c7304de763
Christopher Lie <85123420+LieChristopher@users.noreply.github.com>
2024-08-13 21:01:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from calculate_mangrove_data import Mangrove_data
 
 import ee
 import geemap.foliumap as gee
-import geemap
+import geemap.ee_initialize
 import google.oauth2 as service_account
 
 # ee.Authenticate()
